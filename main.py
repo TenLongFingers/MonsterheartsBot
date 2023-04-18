9b65829e2ce34ad5e1786b4948582e84d09bac2a
Chris McKnight <tenlongfingers10@gmail.com>
2023-04-17 20:58:31 -0600

    dice function added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import discord
+import random
 
-# built on Replit, which has its own system for handling sensitive info
 my_secret = os.environ['TOKEN']
 
 intents = discord.Intents.default()
@@ -14,9 +14,19 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 
+# Check to see if the bot is logged in. Developer use only
 @client.event
 async def on_ready():
   print('We have logged in successfully as {0.user}'.format(client))
+
+
+# Dice roller function
+@client.event
+async def roll_dice():
+  dice_1 = random.randint(1, 6)
+  dice_2 = random.randint(1, 6)
+  total = dice_1 + dice_2
+  return total, dice_1, dice_2
 
 
 @client.event
@@ -31,6 +41,11 @@ async def on_message(message):
     await message.channel.send(
       "Hello, world! This will eventually be updated with instructions on how to get instructions!"
     )
+
+  if msg.startswith("!roll"):
+    total, dice_1, dice_2 = await roll_dice()
+    await message.channel.send("You rolled " + str(dice_1) + " and " +
+                               str(dice_2) + " for a total of " + str(total))
 
 
 client.run(os.getenv('TOKEN'))
