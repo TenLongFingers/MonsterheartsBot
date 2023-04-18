dceab70c4df7e0e557f456983013d9bc55672017
Chris McKnight <tenlongfingers10@gmail.com>
2023-04-17 21:49:06 -0600

    added super success results

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-exclamations = [
+exclamations_array = [
   "WOW!", "HOLY SH*T!", "F*CK YEAH!", "HOT DAMN!", "ya-YEET!", "POG!", "(⊙０⊙)",
   "ᕦ༼ ˵ ◯ ਊ ◯ ˵ ༽ᕤ"
 ]
@@ -34,6 +34,10 @@ async def roll_dice():
   return total, dice_1, dice_2
 
 
+# Random exclamations for super successes
+exclamation = random.choice(exclamations_array)
+
+
 @client.event
 async def on_message(message):
   if message.author == client.user:
@@ -52,13 +56,15 @@ async def on_message(message):
     total, dice_1, dice_2 = await roll_dice()
     result = "You rolled " + str(dice_1) + " and " + str(
       dice_2) + " for a total of " + str(total)
-    # Messages for different rolls
+    # Messages for different rolls TODO: these will probably have to be moved to a different function so people can add stats. It'll have to be restructured.
     if total == 2:
       result = ("(ಥ_ʖಥ)... ") + result + ("! FAIL!")
     elif 3 <= total <= 6:
       result = ("FAIL! ") + result + (".")
     elif 7 <= total <= 9:
-      result = ("Success! " + result + ("."))
+      result = ("Success! ") + result + (".")
+    elif total > 9:
+      result = (exclamation + " " + result + ("! SUCCESS!!"))
     await message.channel.send(result)
 
 
