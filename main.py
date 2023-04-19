c6adb07276bee5c064af4b3ade1dd8de4e9a9eb3
Chris McKnight <tenlongfingers10@gmail.com>
2023-04-19 10:36:11 -0600

    added a prefix variable instead of using the full command for each function. Added notes for future todos.

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+prefix = "!"
 
 exclamations_array = [
   "WOW!", "HOLY SH*T!", "F*CK YEAH!", "HOT DAMN!", "ya-YEET!", "POG!", "(⊙０⊙)",
@@ -25,7 +26,7 @@ async def on_ready():
   print('We have logged in successfully as {0.user}'.format(client))
 
 
-# Dice roller function
+# Dice roller function TODO: add the stats as a callback function. Then you can do an if/elif statements in the roll function, so they can choose whether or not to roll stats
 @client.event
 async def roll_dice():
   dice_1 = random.randint(1, 6)
@@ -46,21 +47,21 @@ async def on_message(message):
   msg = message.content
 
   # TODO: reformat this to have instructions
-  if msg.startswith("!hello"):
+  if msg.startswith(prefix + "hello"):
     await message.channel.send(
       "Hello, world! This will eventually be updated with instructions on how to get instructions!"
     )
 
   #Rolls a simple 2d6
-  if msg.startswith("!roll"):
+  if msg.startswith(prefix + "roll"):
     total, dice_1, dice_2 = await roll_dice()
     result = "You rolled " + str(dice_1) + " and " + str(
       dice_2) + " for a total of " + str(total)
     # Messages for different rolls TODO: these will probably have to be moved to a different function so people can add stats. It'll have to be restructured.
     if total == 2:
-      result = ("(ಥ_ʖಥ)... ") + result + ("! FAIL!")
+      result = (":poop: ... ") + result + ("! FAIL!")
     elif 3 <= total <= 6:
-      result = ("FAIL! ") + result + (".")
+      result = ("Fail! ") + result + (".")
     elif 7 <= total <= 9:
       result = ("Success! ") + result + (".")
     elif total > 9:
