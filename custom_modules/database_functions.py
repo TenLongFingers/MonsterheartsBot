fa4d1c2d7a82b785b3f8d9c2f71ddd59882f0b6c
Chris McKnight <tenlongfingers10@gmail.com>
2023-04-26 10:02:02 -0600

    finally has write permissions to database and can add characters

--- custom_modules/database_functions.py
+++ custom_modules/database_functions.py
@@ -1,52 +1,57 @@
 import os
 
 import pandas as pd
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, MetaData, Table
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
 
+#PERMISSIONS
 CONNECTION_STRING = os.environ['CONNECTION_STRING']
 
-engine = create_engine(CONNECTION_STRING, pool_pre_ping=True)
-
+engine = create_engine(CONNECTION_STRING, isolation_level="AUTOCOMMIT", pool_pre_ping=True)
 
 # Gets all characters and their stat block
 def get_characters():
-    sql = '''
+  sql = '''
     SELECT *
     FROM "characters";
     '''
-    with engine.connect() as conn:
-        df = pd.read_sql(sql, conn)
-    return df
+  with engine.connect() as conn:
+    df = pd.read_sql(sql, conn)
+  return df
 
 
 # Gets single character stat block
 # TODO: error message doesn't print. Probably needs to be handled on main, anyway
 def get_character_stats(name):
-    sql = text('''
+  sql = text('''
     SELECT *
     FROM "characters"
     WHERE "first_name" = :name;
     ''')
-    try:
-        with engine.connect() as conn:
-            df = pd.read_sql(sql, conn, params={'name': name})
-        return df
-    except NoResultFound:
-        print(f"Error: couldn't find {name} in character list. Please check to make sure your spelling is correct.")
+  try:
+    with engine.connect() as conn:
+      df = pd.read_sql(sql, conn, params={'name': name})
+    return df
+  except NoResultFound:
+    print(
+      f"Error: couldn't find {name} in character list. Please check to make sure your spelling is correct."
+    )
 
 
 # Add new character
 def add_new_character(character):
-    try:
-        with engine.connect() as conn:
-            conn.execute(text('''
+  try:
+    with engine.connect() as conn:
+      conn.execute(
+        text('''
                 INSERT INTO characters (first_name, last_name, skin, level, hot, cold, volatile, dark)
                 VALUES (:first_name, :last_name, :skin, :level, :hot, :cold, :volatile, :dark)
             '''), character)
-        return True
-    except SQLAlchemyError:
-        print(f"Error: couldn't find {character['first_name']} in character list. Please check to make sure your spelling is correct.")
-        return False
-
+    return True
+  except SQLAlchemyError:
+    print(
+      f"Error: couldn't find {character['first_name']} in character list. Please check to make sure your spelling is correct."
+    )
+    return False
